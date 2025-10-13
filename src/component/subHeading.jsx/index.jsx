0086ed26018875c0b27c460b8b9bb23738dec770
saima zulfiqar <saimazulfiqarawan3@gmail.com>
2025-10-13 11:56:19 -0700

    toggle button work

--- src/component/subHeading.jsx/index.jsx
+++ src/component/subHeading.jsx/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 const Subheading = () => {
